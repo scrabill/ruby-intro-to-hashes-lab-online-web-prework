0252ee43511f73a459fbd646eb44b3fef0d3bba9
Shannon V. Crabill <shannon.crabill@gmail.com>
2019-06-03 16:09:28 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -29,9 +29,9 @@ def monopoly_with_third_tier
       :names => {},
       :rent_in_dollars => {
         :one_piece_owned => 25,
-        :two_piece_owned => 25,
-        :three_piece_owned => 25,
-        :four_piece_owned => 25
+        :two_piece_owned => 50,
+        :three_piece_owned => 100,
+        :four_piece_owned => 200
       }
     }
   }
